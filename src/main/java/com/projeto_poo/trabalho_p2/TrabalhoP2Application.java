8a3f85956a526740c1eec97bb8c176f0a31488de
Eduardo Barros <edu.barros101@gmail.com>
2025-06-10 18:39:32 -0300

    Primeiro commit

---
+++ src/main/java/com/projeto_poo/trabalho_p2/TrabalhoP2Application.java
@@ -0,0 +1,13 @@
+package com.projeto_poo.trabalho_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoP2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_poo/trabalho_p2/TrabalhoP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_poo.trabalho_p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
