a84ba563a2ef175f5252e8cb4686f82ed3e768c9
Eduardo Barros <edu.barros101@gmail.com>
2025-06-16 18:27:06 -0300

    Adicionando PersonagemService

---
+++ src/main/java/com/projeto_poo/trabalho_p2/model/PersonagemService.java
@@ -0,0 +1,33 @@
+package com.projeto_poo.trabalho_p2.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonagemService {
+
+    @Autowired
+    PersonagemDAO cdao;
+
+    public void inserirPersonagem(Personagem persona) {
+        cdao.inserirPersonagem(persona);
+    }
+
+    public List<Personagem> puxarTodosPersonagems() {
+        return Personagem.converterVarios(cdao.puxarTodosPersonagens());
+    }
+
+    public void atualizarPersonagem(int id, Personagem novo) {
+        cdao.atualizarPersonagem(id, novo);
+    }
+
+    public Personagem puxarPersonagem(int id) {
+        return Personagem.converter(cdao.puxarPersonagem(id));
+    }
+
+    public void deletar(int id) {
+        cdao.deletar(id);
+    }
+}
